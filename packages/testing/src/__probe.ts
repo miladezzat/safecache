26e80b263d57f227c463b9a8243cfafa438d990c
Milad E. Fahmy <miladezzat.f@gmail.com>
2026-06-27 19:55:26 +0300

    refactor: remove unused test probe file

--- packages/testing/src/__probe.ts
+++
@@ -1,6 +0,0 @@
-import { createTestCache } from "./index";
-import type { Clock } from "@safecache/core";
-
-const arbitraryClock: Clock = { now: () => 0 };
-// EXPECT a compile error here: clock must be FakeClock | undefined
-createTestCache({ clock: arbitraryClock });
